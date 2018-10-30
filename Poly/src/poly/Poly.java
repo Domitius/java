8c8606ea47623cb6b49c0320b54d38dbe8785f10
burrisg <burrisg@DT039985.otc.edu>
2018-10-30 12:53:18 -0500

    Created Classes

--- Poly/src/poly/Poly.java
+++ Poly/src/poly/Poly.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package poly;
-
+import java.util.ArrayList;
 /**
  *
  * @author burrisg
@@ -15,7 +15,25 @@ public class Poly {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Polymorphism & Inherit
+        
+        Lion lion = new Lion(6, "Yellow");
+        Dog dog = new Dog(7, "White");    
+        Fish fish = new Fish(100, "Orange");
+                
+        ArrayList<Animal> arrayAnimal = new ArrayList<Animal>();
+        
+        arrayAnimal.add(lion);
+        arrayAnimal.add(dog);
+        arrayAnimal.add(fish);
+        
+        for (int i = 0; i < arrayAnimal.size(); i++) {
+            arrayAnimal.get(i).display();
+            allSpeak(arrayAnimal.get(i));
+        }
     }
-    
+  
+    public static void allSpeak(Animal animal) {
+        animal.speak();
+    }
 }
